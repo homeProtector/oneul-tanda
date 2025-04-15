55d676dca16c5335973ea57901ccbeb9c6ad8b3e
kss <kos7114@naver.com>
2025-04-15 15:43:40 +0900

    fix: kafka setting fix
    현재 kafka event 관련한 메시지 생성에 관한 문제 해결중입니다.

--- queue-service/src/main/java/com/sparta/queueservice/config/KafkaConfig.java
+++ queue-service/src/main/java/com/sparta/queueservice/config/KafkaConfig.java
@@ -1,6 +1,6 @@
 package com.sparta.queueservice.config;
 
-import com.sparta.queueservice.infrastructure.Kafka.event.ReservationHeldEvent;
+import com.sparta.queueservice.infrastructure.kafka.event.ReservationHeldEvent;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,8 +21,13 @@ public class KafkaConfig {
         Map<String, Object> configProps = new HashMap<>();
         configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        return new DefaultKafkaProducerFactory<>(configProps);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+        JsonSerializer<ReservationHeldEvent> jsonSerializer = new JsonSerializer<>();
+        jsonSerializer.setAddTypeInfo(false);
+
+        return new DefaultKafkaProducerFactory<>(configProps, new StringSerializer(), jsonSerializer);
+//        return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     @Bean
