4b07a49c37aec19e374de5f02eebeb440791a27c
oyeonjoo <oyj0842@naver.com>
2025-04-18 03:42:08 +0900

    feat: 직렬화와 역직렬화를 위한 ObjectMapper 설정

---
+++ flight-service/src/main/java/com/oneul_tanda/flight_service/infrastructure/config/JacksonConfig.java
@@ -0,0 +1,21 @@
+package com.oneul_tanda.flight_service.infrastructure.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());// Java 8 날짜 모듈 등록
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // ISO-8601로 출력
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false); // 알 수 없는 속성 무시
+        return objectMapper;
+    }
+}
