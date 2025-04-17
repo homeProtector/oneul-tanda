526c6151f8350d1c54d7c9ddf82082bef60d6198
kss <kos7114@naver.com>
2025-04-17 15:30:27 +0900

    feat: tokenVersion redis blacklist
    gateway에서 redistokenVersion을 통한 blacklist 기능 구현

--- gateway-service/src/main/java/com/oneultanda/gatewayservice/infrastructure/kafka/KafkaConsumerService.java
+++ gateway-service/src/main/java/com/oneultanda/gatewayservice/infrastructure/kafka/KafkaConsumerService.java
@@ -2,13 +2,22 @@ package com.oneultanda.gatewayservice.infrastructure.kafka;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class KafkaConsumerService {
+
+    private final RedisTemplate<String, String> redisTemplate;
+
+    @Value("${tokenVersion-expiration}")
+    private int toKenVersionExpiration;
 
     @KafkaListener(topics = KafkaTopics.TOKEN_VERSION_CHANGED, groupId = "gateway_group")
     public void handleDeliveryStatusChanged(TokenVersionChangedEvent event) {
@@ -17,6 +26,8 @@ public class KafkaConsumerService {
         try {
             // redis 처리
             log.info("{}의 토큰버전이 {}로 변경. redis 처리가 필요합니다!", event.userId(), event.tokenVersion());
+            String key = "token_version:" + event.userId();
+            redisTemplate.opsForValue().set(key, String.valueOf(event.tokenVersion()), Duration.ofMinutes(toKenVersionExpiration));
 
         } catch(Exception e) {
             // todo: 실패에 대한 보상처리 과정
