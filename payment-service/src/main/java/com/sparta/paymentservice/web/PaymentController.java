92d44769a49548c7996dc81e8097a1a9b1a4c900
earthkid0 <ghs1541@naver.com>
2025-04-25 19:06:02 +0900

    refactor/구현체가 아닌 인터페이스 주입

--- payment-service/src/main/java/com/sparta/paymentservice/web/PaymentController.java
+++ payment-service/src/main/java/com/sparta/paymentservice/web/PaymentController.java
@@ -3,23 +3,26 @@ package com.sparta.paymentservice.web;
 import com.siot.IamportRestClient.request.CardInfo;
 import com.sparta.paymentservice.application.dto.PaymentRequestDto;
 import com.sparta.paymentservice.application.dto.PaymentResponseDto;
-import com.sparta.paymentservice.application.service.KcpPaymentService;
+import com.sparta.paymentservice.application.service.PaymentService;
 import com.sparta.paymentservice.common.util.TestCardInfo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/payments")
 @RequiredArgsConstructor
 public class PaymentController {
-    private final KcpPaymentService kcpPaymentService;
+    private final PaymentService paymentService;
 
     @PostMapping("/confirm")
     public ResponseEntity<PaymentResponseDto> confirmPayment(@RequestBody PaymentRequestDto request) {
         CardInfo card = TestCardInfo.testCard1();
-        PaymentResponseDto response = kcpPaymentService.confirmPayment(request, card);
+        PaymentResponseDto response = paymentService.confirmPayment(request, card);
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
