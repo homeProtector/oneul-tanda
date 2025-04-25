1ff877271e14131a1a749b871e812ced5c844300
earthkid0 <ghs1541@naver.com>
2025-04-25 10:26:37 +0900

    fix/jpa 와 동일한 타입으로 변경

--- payment-service/src/main/java/com/sparta/paymentservice/domain/repository/PaymentRepository.java
+++ payment-service/src/main/java/com/sparta/paymentservice/domain/repository/PaymentRepository.java
@@ -3,5 +3,5 @@ package com.sparta.paymentservice.domain.repository;
 import com.sparta.paymentservice.domain.entity.Payments;
 
 public interface PaymentRepository {
-    void save(Payments payments);
+    Payments save(Payments payments);
 }
