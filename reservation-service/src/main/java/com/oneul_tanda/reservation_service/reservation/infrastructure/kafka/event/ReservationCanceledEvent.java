18cdd716138cabe97ab68034e38738324841ecb1
jin2304 <search2304@naver.com>
2025-05-07 15:24:37 +0900

    feat: 예약 취소 이벤트 정의 및 예약 취소 Kafka 프로듀서 구현

---
+++ reservation-service/src/main/java/com/oneul_tanda/reservation_service/reservation/infrastructure/kafka/event/ReservationCanceledEvent.java
@@ -0,0 +1,50 @@
+package com.oneul_tanda.reservation_service.reservation.infrastructure.kafka.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReservationCanceledEvent {
+    private UUID eventId;                   // 대상: 이벤트 고유 ID or 이벤트 대상 ID
+    private String eventType;               // 행위: 이벤트 타입
+    private LocalDateTime reservationTime;  // 시간: 행위 발생 시각
+    private Data data;                      // 정보: 행위와 관련된 정보
+
+    @Getter
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
+        private UUID flightId;
+        private UUID userId;
+        private Integer seatCount;
+    }
+
+
+    public static ReservationCanceledEvent of(UUID reservationId,
+                                              UUID flightId,
+                                              UUID userId,
+                                              Integer seatCount,
+                                              String eventType) {
+
+        return ReservationCanceledEvent.builder()
+                .eventId(reservationId)
+                .eventType(eventType)
+                .reservationTime(LocalDateTime.now())
+                .data(ReservationCanceledEvent.Data.builder()
+                        .flightId(flightId)
+                        .userId(userId)
+                        .seatCount(seatCount)
+                        .build())
+                .build();
+    }
+}
+
