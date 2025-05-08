6a2dc04d6ca8fa4f677526788090f6edf827f1e1
kss <kos7114@naver.com>
2025-05-08 18:47:20 +0900

    fix: topic name match
    토픽명이 일치 하지 않는 문제 해결

--- websocket-service/src/main/java/com/oneul_tanda/websocket_service/infrastructure/kafka/KafkaTopics.java
+++ websocket-service/src/main/java/com/oneul_tanda/websocket_service/infrastructure/kafka/KafkaTopics.java
@@ -1,7 +1,7 @@
 package com.oneul_tanda.websocket_service.infrastructure.kafka;
 
 public class KafkaTopics {
-    public static final String WEBSOCKET_MESSAGE = "websocket.notification-message";
+    public static final String WEBSOCKET_MESSAGE = "reservation-pending";
 }
 
 
