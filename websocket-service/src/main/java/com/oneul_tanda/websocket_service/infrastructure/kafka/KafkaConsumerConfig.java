4d7452a480df9800225d38a38b0f672ad5591fb1
kss <kos7114@naver.com>
2025-05-09 12:51:15 +0900

    fix: kafka desirialize error
    타입 일치화

--- websocket-service/src/main/java/com/oneul_tanda/websocket_service/infrastructure/kafka/KafkaConsumerConfig.java
+++ websocket-service/src/main/java/com/oneul_tanda/websocket_service/infrastructure/kafka/KafkaConsumerConfig.java
@@ -1,7 +1,7 @@
 package com.oneul_tanda.websocket_service.infrastructure.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +14,6 @@ import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 @EnableKafka
 @Configuration
@@ -24,7 +23,7 @@ public class KafkaConsumerConfig {
     String bootstrapServers;
 
     @Bean
-    public ConsumerFactory<UUID, ReservationHeldEvent> consumerFactory() {
+    public ConsumerFactory<String, ReservationHeldEvent> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
 
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
@@ -34,20 +33,20 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
 
         // 실제 사용할 Deserializer 지정
-        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, UUIDDeserializer.class);
+        props.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class);
         props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class);
 
         // JsonDeserializer 설정
         props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
-        props.put(JsonDeserializer.TYPE_MAPPINGS, "TokenVersionChangedEvent:com.oneul_tanda.websocket_service.infrastructure.kafka.ReservationHeldEvent");
+        props.put(JsonDeserializer.TYPE_MAPPINGS, "ReservationHeldEvent:com.oneul_tanda.websocket_service.infrastructure.kafka.ReservationHeldEvent");
 
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
 
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<UUID, ReservationHeldEvent> kafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<UUID, ReservationHeldEvent> factory =
+    public ConcurrentKafkaListenerContainerFactory<String, ReservationHeldEvent> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, ReservationHeldEvent> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
         return factory;
